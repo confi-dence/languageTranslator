ea7f68c7f5923e11f3d6d8383e01f5fe7d0d9a31
Confidence <confi2s7@gmail.com>
2025-10-14 07:38:36 +0100

    language Translateor first commit

---
+++ langauage.js
@@ -0,0 +1,46 @@
+const openFrom = document.getElementById('openFrom'),
+opento = document.getElementById('opento'),
+closeFrom = document.getElementById('closeFrom'),
+closeTo = document.getElementById('closeTo'),
+usertype = document.getElementById('usertype'),
+langauage = document.querySelectorAll('langauage')
+
+
+openFrom.addEventListener('click', function () {
+    closeFrom.style.display = 'flex';
+    usertype.style.display = 'none';
+    // updateUsertypeVisibility();
+  });
+  
+  closeFrom.addEventListener('click', function () {
+    closeFrom.style.display = 'none';
+    usertype.style.display = 'flex';
+    updateUsertypeVisibility();
+  });
+  
+  opento.addEventListener('click', function () {
+    closeTo.style.display = 'flex';
+    usertype.style.display = 'none';
+    // updateUsertypeVisibility();
+  });
+  
+  closeTo.addEventListener('click', function () {
+    closeTo.style.display = 'none';
+    usertype.style.display = 'flex';
+    updateUsertypeVisibility();
+  });
+  
+  function updateUsertypeVisibility() {
+    // show usertype only if both lists are hidden
+    if (closeFrom.style.display === 'none' && closeTo.style.display === 'none') {
+        usertype.style.display = 'flex';
+    }else {
+      usertype.style.display = 'none';
+    }
+  }
+function changeNameFrom(newname) {
+    openFrom.innerText = newname
+}
+function changeNameTo(newname) {
+    opento.innerText = newname
+}
