e6d5addbe201e4be4c27d6be5b78dff59c7df92f
Confidence <confi2s7@gmail.com>
2025-10-21 03:50:07 +0100

    Removed API key for security

--- langauage.js
+++ langauage.js
@@ -3,7 +3,13 @@ opento = document.getElementById('opento'),
 closeFrom = document.getElementById('closeFrom'),
 closeTo = document.getElementById('closeTo'),
 usertype = document.getElementById('usertype'),
-langauage = document.querySelectorAll('langauage')
+langauage = document.querySelectorAll('langauage'),
+
+sourceLang = document.getElementById('sourceLang'),
+TargetLang = document.getElementById('TargetLang'),
+inputedText = document.getElementById('inputedText'),
+OutputedText = document.getElementById('OutputedText'),
+triggerButton = document.getElementById('triggerButton')
 
 
 openFrom.addEventListener('click', function () {
@@ -44,3 +50,40 @@ function changeNameFrom(newname) {
 function changeNameTo(newname) {
     opento.innerText = newname
 }
+
+triggerButton.addEventListener('click', function () {
+  const text = inputedText.value.trim()
+  if(!text){
+    OutputedText.value = "";
+    return
+  }else{
+    OutputedText.innerText = 'Translating......'
+  }
+  const usermessage = {
+    q: text,
+    source :sourceLang,
+target: TargetLang,
+format: 'text'
+  }
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      "Authorization": `Bearer ${apiKey}`, 
+    },
+    body: JSON.stringify(usermessage)
+  }
+  fetch(apiUrl, requestOptions).then(
+    response =>{
+if (!response.ok) {
+  throw new Error('network response was not ok')
+}
+return response.json()
+    }
+  ).then(data => {
+    OutputedText.innerText = data.translatedText || 'Translation failed'
+  }).catch(error => {
+    console.error('error', error);
+    OutputedText.innerText = 'An error occurred';
+  })
+})  
