b759a660e0e8cefcac684c7df90f269c6b63a5cc
Confidence <confi2s7@gmail.com>
2025-10-25 12:13:50 +0100

    jdjd

--- langauage.js
+++ langauage.js
@@ -146,11 +146,11 @@ speaker.addEventListener('click', () => {
 
 // As the person speaks, text appears in the textarea
 recognition.onresult = (event) => {
-  let transcript = '';
+  // let transcript = '';
   for (let i = 0; i < event.results.length; i++) {
     transcript += event.results[i][0].transcript;
   }
-  inputedText.value = transcript;
+  // inputedText.value = transcript;
 };
 
 // Optional: handle errors quietly
